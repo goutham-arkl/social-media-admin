0d198a851814b633d18eee7c056103ff8ec9bcb4
goutham-arkl <gouthemarkl@gmail.com>
2023-02-10 19:27:37 +0530

    final commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://socket.nutranation.ml',
+      target: 'https://server.nutranation.ml',
       changeOrigin: true,
     })
   );
